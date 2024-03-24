5ae139b21007aedec3e44e6afdae627a0d53424f
codekakitai51 <codekakitai51@gmail.com>
2024-03-24 13:43:33 +0900

    add new algo

---
+++ trees/DFS/39. Combination Sum/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            elif total > target or i >= len(candidates):
+                return
+            
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+        return res
+    
+# shallow copy vs deep copy
+    # shallow copy: copy.copy() -> only copies the references to the objects
+    # deep copy: copy.deepcopy() -> copies the objects themselves
