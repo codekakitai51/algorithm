21ce98895fd174884544e6b24e9fcca52aa841ec
codekakitai51 <codekakitai51@gmail.com>
2024-05-18 10:57:47 +0900

    add new algo

--- trees/DFS/416. Partition Equal Subset Sum/solution.py
+++ trees/DFS/416. Partition Equal Subset Sum/solution.py
@@ -4,7 +4,6 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total_sum = sum(nums)
         
-        # 総和が奇数の場合、等しい部分集合に分けることはできません。
         if total_sum % 2 != 0:
             return False
         
@@ -14,16 +13,16 @@ class Solution:
         memo = {}
 
         def dfs(index, current_sum):
-            if current_sum == target:  # 目標値に達した場合はTrue
+            if current_sum == target:
                 return True
-            if current_sum > target or index == n:  # 範囲外または合計が目標を超えた場合はFalse
+            if current_sum > target or index == n:  
                 return False
-            if (index, current_sum) in memo:  # メモ化された結果を利用
+            if (index, current_sum) in memo:  
                 return memo[(index, current_sum)]
             
-            # 現在の要素を含める場合と含めない場合の結果を計算
+            #  ↓return True immediately if either branch returns True↓
             result = dfs(index + 1, current_sum + nums[index]) or dfs(index + 1, current_sum)
-            memo[(index, current_sum)] = result  # 結果をメモ化
+            memo[(index, current_sum)] = result
             return result
         
         return dfs(0, 0)
