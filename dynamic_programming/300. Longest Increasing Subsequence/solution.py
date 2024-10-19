679c17f4c3f9dab6cd708a7b994153bc48d62f9a
codekakitai51 <codekakitai51@gmail.com>
2024-10-19 12:38:00 +0900

    add new algo

--- dynamic_programming/300. Longest Increasing Subsequence/solution.py
+++ dynamic_programming/300. Longest Increasing Subsequence/solution.py
@@ -1,11 +1,26 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * len(nums)
-        for i in range(1, len(nums)):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        
-        return dp[-1]
+        maxLen = 1
+        subArr = [nums[0]] # subArr is always sorted
 
-    
+        def BS(arr, targetNum):
+            l, r = 0, len(arr) - 1
+            while l < r:
+                mid = (l + r) // 2
+                if targetNum <= arr[mid]:
+                    r = mid
+                else:
+                    l = mid + 1
+                
+            return l
+
+        for num in nums[1:]:
+            if subArr[-1] < num:
+                subArr.append(num)
+            else:
+                # find num <=, and remove all using binary search library
+                # targetIdx = bisect.bisect_left(subArr, num)
+                targetIdx = BS(subArr, num)
+                subArr[targetIdx] = num
+        
+        return len(subArr)
