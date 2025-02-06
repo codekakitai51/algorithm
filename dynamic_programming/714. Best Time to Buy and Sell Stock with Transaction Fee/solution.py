164022018f0a659278a99bd225c0c922936886a0
codekakitai51 <codekakitai51@gmail.com>
2025-02-06 10:42:22 +0900

    add new algo

---
+++ dynamic_programming/714. Best Time to Buy and Sell Stock with Transaction Fee/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        free, hold = [0] * n, [0] * n
+        hold[0] = -prices[0]
+
+        for i in range(1, n):
+            free[i] = max(free[i - 1], hold[i - 1] + prices[i] - fee)
+            hold[i] = max(hold[i - 1], free[i - 1] - prices[i])
+        
+        return free[-1]
