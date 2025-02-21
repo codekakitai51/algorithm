ff0da0ad1ada6004fb8ef85938f3a3a54cc82fd3
codekakitai51 <codekakitai51@gmail.com>
2025-02-21 13:35:54 +0900

    add new algo

---
+++ array/2422. Merge Operations to Turn Array Into a Palindrome/solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        operations = 0
+
+        while l < r:
+            if nums[l] < nums[r]:
+                nums[l + 1] += nums[l]
+                l += 1
+                operations += 1
+            elif nums[l] > nums[r]:
+                nums[r - 1] += nums[r]
+                r -= 1
+                operations += 1
+            else:
+                l += 1
+                r -= 1
+
+        return operations
